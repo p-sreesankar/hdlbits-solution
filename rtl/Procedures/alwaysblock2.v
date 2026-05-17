37d4a36988f0ea71e7deb81ff00d9c079129d211
p-sreesankar <aeu250709@cet.ac.in>
2026-05-17 22:53:07 +0530

    updated alwaysblock2.v

--- rtl/Procedures/alwaysblock2.v
+++ rtl/Procedures/alwaysblock2.v
@@ -9,6 +9,6 @@ module top_module(
     
     assign out_assign = a ^ b;
     always @(*) out_always_comb = a ^ b;
-    always @(posedge clk) out_always_ff = a ^ b;
+    always @(posedge clk) out_always_ff <= a ^ b;
 
 endmodule
